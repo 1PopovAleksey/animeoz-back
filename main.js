2e749cd32aa493aa98b070535e3ef7972397d007
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-02-14 01:07:07 +0300

    feat: update main.js

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\nI alive');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-server.listen(port, hostname, () => {
+app.get('/new', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send([
+    {
+      id: 1,
+      name: 'Стикер аниме',
+      category: 'sticker',
+    },
+    {
+      id: 2,
+      name: 'Кружка аниме',
+      category: 'cup',
+    },
+    {
+      id: 3,
+      name: 'Брелки аниме',
+      category: 'keychain',
+    },
+  ]);
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
